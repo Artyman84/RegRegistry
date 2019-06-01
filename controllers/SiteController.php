5eb9f842195bc38791af0e8387bb7770f95a44f7
Artiom Iacovetz <arty_itweb@mail.ru>
2019-06-01 15:47:54 +0300

    cleaning

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -15,21 +15,6 @@ class SiteController extends Controller
     /**
      * {@inheritdoc}
      */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function actions()
     {
         return [
